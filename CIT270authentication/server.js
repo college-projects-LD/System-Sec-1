8dada9cc33b6e30cceb23b16081d5dccd8fd3ed5
Landon Delaney <landondelaney@byui.edu>
2022-05-20 13:01:20 -0600

    adding comments and now the code works now

--- CIT270authentication/server.js
+++ CIT270authentication/server.js
@@ -1,50 +1,51 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const req = require('express/lib/request');
-const port = 3000 ;
+
+const port = 3000;
 const app = express();
 const md5 = require('md5');
-const redis = require('redis');
-const redisClient = redis.createClient();
 
-app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+const {createClient} = require('redis');
+const redisClient = createClient({
+socket:{
+    port: 6379,
+    host: '127.0.0.1'
+}
 
-app.listen(port, () => {
-    console.log('Server is listening on port ' + port);}
+}//for things with promis you need to add an await
+);
+
+redisClient.connect();// make the TCP connection to the redis server
+
+app.use(bodyParser.json());// use the middleware
+
+app.listen(port, async, () => {
+    console.log(`Server is listening on port ${port}`);}
     );
 
-    app.get('/', (request,response)=> {
-        response.send("Hello");
-});
+// read a password from redis 
+const validatePassword = async (request, response)=>{
+    const requestHashedPassword = md5(request.body.password); //get the passwprd from the request and hash it
+    const redisHashedPassword = await redisClient.hmGet('passwords',request.body.userName); //get the hash from redis
+    const loginRequest = request.body;
+    console.log("request Body",JSON.stringify(request.body));
+    //search the database for the username, if it exists, get the password
 
-app.post('/login',async (request,response)=> { //a post request is used by a client to send data to the server
-    const loginRequest = request.body; 
-    console.log("request.body", JSON.stringify(request.body));
-    // search the redis datbase for username and retreive current password in the database
-    const redisHashedPassword= await redisClient.hGet('passords', loginRequest.password);
-    const hahedPassword = md5(loginRequest.password);
-    console.log("redisHashedPassword", redisHashedPassword);
-    if (redisHashedPassword === hahedPassword) {
-        response.send("Login Successful");
-    } else {
-        response.send("Login Failed");
+    //compare the hashed password with the hashed password in the database
+    if( requestHashedPassword == redisHashedPassword){
+        response.status(200);//if the passwords match, send a response 200 OK
+        response.send(`${loginRequest.userName} is logged in Welcome!`);
     }
+    else{
+        response.status(401);//if the passwords don't match, send a response 401 Unauthorized
+        response.send(`Unauthorized`);
+    }
+
+
+}
+
+app.get('/', (request, response) => {
+    response.send('Hello');
 });
 
-
-
-
-    // compare password with password sent by client with the hased version in the redis database
-
-//const hahedPassword = md5(loginRequest.password);
-
-    /*if (loginRequest.userName === "landondelaney@byui.edu" && loginRequest.password === "KJhgfdsa123") {
-        response.status(200);
-        response.send("Welcome");
-
-    } else {
-        response.status(401);
-        response.send("Unauthorized");
-        
-    }*/
- 
+app.post('/login', validatePassword);
