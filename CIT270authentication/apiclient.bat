5856c7f66ea541486862ac29a3834a1a010d7ef1
Landon Delaney <landondelaney@byui.edu>
2022-07-15 12:46:33 -0600

    changing the url

--- CIT270authentication/apiclient.bat
+++ CIT270authentication/apiclient.bat
@@ -1,1 +1,1 @@
-curl -d "@data.json" -X POST -H "Content-Type:application/json" https://landondelaney-kubenodejs.cit270.com/login
+curl -d "@data.json" -X POST -H "Content-Type:application/json" https://landondelaney-kubenodejs.cit270.com/signup
